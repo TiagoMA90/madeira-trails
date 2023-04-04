ae4648f5862f96b083ec7c33e1c19b1baf8ea5e4
TiagoMA90 <Tiago_MA90@hotmail.com>
2023-04-04 11:16:45 +0000

    fix typo on line 132 from settings.py

--- madeiratrails/settings.py
+++ madeiratrails/settings.py
@@ -129,7 +129,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_STORAGE = 'cloudinary_storage.storage.StaticHashedCloudinaryStorage'
-STATICFILES_DIRS = [os.path.join(BASE.DIR, 'staticfiles')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
 
 MEDIA_URL = '/media/'
 DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
