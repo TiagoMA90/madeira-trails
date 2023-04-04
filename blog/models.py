e9579dd73fb7309c452323a2599d4c3024d67085
TiagoMA90 <Tiago_MA90@hotmail.com>
2023-04-04 23:15:20 +0000

    apply fixes

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import Cloudinary
+
+STATUS = ((0, "Draft"), (1, "Published"))
 
 # Create your models here.
