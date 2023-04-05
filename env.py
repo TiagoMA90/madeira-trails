3a9dfd224030816e1d27c9d4f5586cb24e2beb91
TiagoMA90 <Tiago_MA90@hotmail.com>
2023-04-05 21:01:02 +0000

    remove env.py from gitignore

---
+++ env.py
@@ -0,0 +1,5 @@
+import os
+
+os.environ["DATABASE_URL"] = "postgres://tdsldomq:mHQ9iTVEuxzSYPCE46_p5XzNISuDfwQk@trumpet.db.elephantsql.com/tdsldomq"
+os.environ["SECRET_KEY"] = "e157d994-4ce0-4ea6-8a2c-c6256d00aa70"
+os.environ["CLOUDINARY_URL"] = "cloudinary://572555385744258:0COw3jIp0nWL_2Lzjn7KTeGgihM@dmbdqco85"
