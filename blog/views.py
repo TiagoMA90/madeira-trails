c2780e9a47ed7b9fb303d20a57ff10bab834fe48
TiagoMA90 <Tiago_MA90@hotmail.com>
2023-04-07 11:32:06 +0000

    apply generic ListView and create templates, media and static/css folders

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from django.views import generic
+from .models import Post
 
-# Create your views here.
+# Class inheriting from Generic ListView
+class PostList(generic.ListView):
+    model = Post
+    # Only published Posts visible to Users ergo 1
+    queryset = Post.objects.filter(status=1).order_by('-created_on')
+    # Renders on the index.html
+    template_name='index.html'
+    # Separates the number of Posts by 6
+    paginate_by = 6
