ccba4be5b76c0af8808592e2aa3f261c827a8241
TiagoMA90 <Tiago_MA90@hotmail.com>
2023-04-07 11:18:04 +0000

    reinforce the classes properties and fix typo

--- blog/admin.py
+++ blog/admin.py
@@ -3,9 +3,10 @@ from .models import Post
 from .models import Comment
 from django_summernote.admin import SummernoteModelAdmin
 
+# Class for the Post
 @admin.register(Post)
 class PostAdmin(SummernoteModelAdmin):
-    # Displays the properties (title/slug/status/created date)
+    # Displays the properties for the Post (title/slug/status/created date)
     list_display = ('title', 'slug', 'status', 'created_on')
     # Search field for content and title
     searc_fields = ['title', 'content']
@@ -15,8 +16,17 @@ class PostAdmin(SummernoteModelAdmin):
     list_filter = ('status', 'created_on')
     summernote_fields = ('content')
 
+# Class for the Comment
 @admin.register(Comment)
 class CommentAdmin(admin.ModelAdmin):
+    # Displays the properties for the Comment (name/body/status/created date)
     list_display = ('name', 'body', 'post', 'created_on', 'approved')
     list_filter = ('approved', 'created_on')
-    searcg_fields = ('name', 'email', 'body')
+    # Searchable by the (name/email/body)
+    search_fields = ('name', 'email', 'body')
+    # Approves the comment
+    actions = ['approve_comments']
+
+    # Function for approval of the Comment
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
